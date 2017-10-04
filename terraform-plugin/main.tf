a21722e46c2aaaf2a203662b13817a8e907c7669
danieljgraves <daniel.graves@rig.net>
2017-10-04 14:02:14 -0500

    testing deploy file again

--- terraform-plugin/main.tf
+++ terraform-plugin/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   ami = "${var.ami}"
   key_name = "${var.key_name}"
   vpc_security_group_ids = ["${aws_security_group.web.id}"]
-  subnet_id = "${var.sub3}"
+  subnet_id = "${var.sub2}"
   tags {
         Name = "${var.hostname}"
         Role = "${var.role}"
@@ -13,11 +13,16 @@ resource "aws_instance" "web" {
         PrincipalId = "AIDAJQPTTLIJNWDTZJBBQ"
   }
 
+  provisioner "file" {
+    source      = "~/deploy.php"
+    destination = "${var.web_dir}"
+  }
+
   provisioner "remote-exec" {
     inline = [
 	"sudo apt-get -y update",
 	"echo Installing system updates DONE!",
-	"cd ${var.web_dir} && sudo git remote set-url origin ${var.git_home}.git"
+	"cd ${var.web_dir} && sudo chown www-data:www-data deploy.php && sudo git remote set-url origin ${var.git_home}.git"
     ]
     connection {
         type = "ssh"
