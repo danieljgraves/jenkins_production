4353bd9bfbcb18c201672bb6e6205e3bb64fe308
danieljgraves <daniel.graves@rig.net>
2017-10-04 16:12:24 -0500

    Cleaning up code

--- terraform-plugin/variables.tf
+++ terraform-plugin/variables.tf
@@ -3,29 +3,6 @@ variable "aws_region" {
   default = "us-east-2"
 }
 
-variable "vpc_id" {
-  default = "vpc-02ba436b"
-}
-
-variable "eip" {
-  default = "eipalloc-6fa6cd41"
-}
-
-variable "sub1" {
-  description = "Main subnet for US-East-2 VPC"
-  default = "subnet-83b27bea"
-}
-
-variable "sub2" {
-  description = "Main subnet for US-East-2 VPC"
-  default = "subnet-ea0c1792"
-}
-
-variable "sub3" {
-  description = "Main subnet for US-East-2 VPC"
-  default = "subnet-69bd8423"
-}
-
 variable "availability_zones" {
   default = "us-east-2b,us-east-2c,us-east-2a"
   description = "List of availability zones, use AWS CLI to find your "
