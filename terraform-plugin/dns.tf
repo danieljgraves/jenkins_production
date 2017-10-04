0ea3a78366930bafe362d6afd36b98554418aa77
danieljgraves <daniel.graves@rig.net>
2017-10-04 16:36:32 -0500

    Updating DNS fqdn data

--- terraform-plugin/dns.tf
+++ terraform-plugin/dns.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "site" {
   zone_id = "Z2SH507JHJ792W"
-  name    = "staging.site.dev.rig.net"
+  name    = "${var.env}.site.dev.rig.net"
   type    = "A"
   ttl     = "60"
   records = ["${aws_instance.web.*.public_ip}"]
